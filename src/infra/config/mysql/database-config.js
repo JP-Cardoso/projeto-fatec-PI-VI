b92b489c7f7f69ea46b54afb70d75a90a8d727de
jp-cardoso <jhonpoal@hotmail.com>
2024-06-14 12:24:12 -0300

    .

--- src/infra/config/mysql/database-config.js
+++ src/infra/config/mysql/database-config.js
@@ -2,6 +2,10 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
+
 const database = process.env.DB_DATABASE;
 const host = process.env.DB_HOST;
 const username = process.env.DB_USER;
